8dc79506767c216f74c6fc051813845d08532a33
Genesses Fernandes <genesses12@gmail.com>
2023-06-27 21:02:20 -0400

    iniciando em React

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <ol>
+          <a href='#'>inicio</a>
+          <a href='#'>projetos</a>
+          <a href='#'>Contato</a>
+        </ol>
+
+      <h1>Olá mundo(Para quebrar a Maldição :D)</h1>
+      <p>trajetória de um Sonho</p>
+      <h2>Pode ser um código bobo, porém faz parte da minha trajetória de Aprendizado</h2>
+    </div>
+    
+
+
+  );
+
+
+   }
+
+export default App;
+
+
+
