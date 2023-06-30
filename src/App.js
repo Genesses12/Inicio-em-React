f7b3717f1063d4cec8bf22e645d01f8de3977423
Genesses Fernandes <genesses12@gmail.com>
2023-06-30 11:48:25 -0400

    Meu-primeiro-componente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-
+//comnonentes
+import FirstComponet from './components/FirstComponet';
+//style.css
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-        <ol>
-          <a href='#'>inicio</a>
-          <a href='#'>projetos</a>
-          <a href='#'>Contato</a>
-        </ol>
-
+      <FirstComponet/>
       <h1>Olá mundo(Para quebrar a Maldição :D)</h1>
       <p>trajetória de um Sonho</p>
-      <h2>Pode ser um código bobo, porém faz parte da minha trajetória de Aprendizado</h2>
+      <h2>Pode ser um código bobo, porém faz parte da minha trajetória de Aprendizado :D </h2>
     </div>
-    
-
-
   );
 
 
---
+++ src/components/FirstComponet.js
@@ -0,0 +1,14 @@
+const FirstComponet = () =>{
+    return (
+        <div>
+            <ol>
+          <a href='#'>inicio</a>
+          <a href='#'>projetos</a>
+          <a href='#'>Contato</a>
+        </ol>
+        
+        </div>
+
+    );
+};
+export default FirstComponet;
