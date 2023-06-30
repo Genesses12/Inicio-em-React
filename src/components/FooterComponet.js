c7eb607f98bcfe0351599788fe8c2d7be5525f76
Genesses fernandes <102777715+Genesses12@users.noreply.github.com>
2023-06-30 18:29:30 -0400

    Adicionando componente
    Adicionando alterações

---
+++ src/components/FooterComponet.js
@@ -0,0 +1,12 @@
+import "../App.css"
+const FooterComponet = () => {
+    return (
+
+        <div>
+        <footer>
+            <h1>Todos os direitos reservados</h1>
+        </footer>
+        </div>
+    );
+};
+export default FooterComponet ;
