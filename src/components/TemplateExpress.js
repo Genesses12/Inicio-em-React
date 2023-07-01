4b6deea84c856b118c9ec7bccfa69eb6ac3faac9
Genesses fernandes <102777715+Genesses12@users.noreply.github.com>
2023-07-01 12:07:02 -0400

    Add files via upload

---
+++ src/components/TemplateExpress.js
@@ -0,0 +1,12 @@
+const TemplateExpress = () =>{
+    const name = "Genesses";
+    const idade = 29
+
+    return  (
+        <div>
+            <h1>Bom dia me chamo {name}, tenho {idade}, Tudo bem com vocês ? </h1>
+        </div>
+    )
+
+}
+export default TemplateExpress;
